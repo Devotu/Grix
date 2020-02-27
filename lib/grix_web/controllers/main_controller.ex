287d43281cefd883239f3384395fb8963fbc6f49
DevotuOne <devotu.developer@gmail.com>
2020-02-27 10:09:35 +0100

    Refactor - Remove unused

--- lib/grix_web/controllers/main_controller.ex
+++ lib/grix_web/controllers/main_controller.ex
@@ -1,11 +1,7 @@
 defmodule GrixWeb.MainController do
   use GrixWeb, :controller
 
-  alias Grix.Player
-  alias Grix.Helpers
-
   def show(conn, _params) do
-    IO.inspect(conn.assigns, label: "assigns - main")
     render(conn, "main.html")
   end
 
