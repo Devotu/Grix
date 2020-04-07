e4a0639caeb3071a4ecb4d93698b13b972540246
DevotuOne <devotu.developer@gmail.com>
2020-04-07 23:37:04 +0200

    Game - Create after record

--- lib/grix_web/controllers/game_controller.ex
+++ lib/grix_web/controllers/game_controller.ex
@@ -32,16 +32,16 @@ defmodule GrixWeb.GameController do
 
 
   def record(conn, params) do
-    {:ok, squad_1} = Squad.get(params["squad_1"])
+    {:ok, squad_1} = Squad.get(params["squad_id_1"])
     {:ok, ships_1} = Ship.get_ships_in_squad(squad_1.id)
     complete_squad_1 = Map.put(squad_1, :ships, ships_1)
 
-    {:ok, squad_2} = Squad.get(params["squad_2"])
+    {:ok, squad_2} = Squad.get(params["squad_id_2"])
     {:ok, ships_2} = Ship.get_ships_in_squad(squad_2.id)
     complete_squad_2 = Map.put(squad_2, :ships, ships_2)
 
-    {:ok, player_1} = Player.get(params["player_1"])
-    {:ok, player_2} = Player.get(params["player_2"])
+    {:ok, player_1} = Player.get(params["player_id_1"])
+    {:ok, player_2} = Player.get(params["player_id_2"])
 
     conn
     |> assign(:squad_1, complete_squad_1)
@@ -53,12 +53,12 @@ defmodule GrixWeb.GameController do
 
 
   def create(conn, params) do
-    {:ok, squad_1} = Squad.get(params["squad_1"])
-    {:ok, squad_2} = Squad.get(params["squad_2"])
+    {:ok, squad_1} = Squad.get(params["squad_id_1"])
+    {:ok, squad_2} = Squad.get(params["squad_id_2"])
 
     {:ok, game_id} = Game.create(conn.assigns.player.id)
-    {:ok, score_1_id} = Score.create(params["player_1"], params["squad_1"], game_id, params["points_1"])
-    {:ok, score_2_id} = Score.create(params["player_2"], params["squad_2"], game_id, params["points_2"])
+    {:ok, score_1_id} = Score.create(params["player_id_1"], params["squad_id_1"], game_id, params["points_1"])
+    {:ok, score_2_id} = Score.create(params["player_id_2"], params["squad_id_2"], game_id, params["points_2"])
 
     {:ok, game} = Game.get(game_id)
     {:ok, score_1} = Score.get(score_1_id)
